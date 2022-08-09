158dc4cb2f6b22eb59060c6f76ee021e92b48bf5
jh000107 <junhuicho8@gmail.com>
2022-08-09 20:10:49 +0900

    testing functions

--- CS50_SEARCH/tictactoe/test.py
+++ CS50_SEARCH/tictactoe/test.py
@@ -1,11 +1,6 @@
-X = "X"
-O = "O"
-EMPTY = None
+from tictactoe import *
 
-initial = [[EMPTY, EMPTY, EMPTY],
+board_1 = initial_state()
+board_2 = [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
-
-tuple = (1,2)
-i,j = tuple
-print(i,j)
